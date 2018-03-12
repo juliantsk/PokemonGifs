f6a15a65fb493e94ee46a867d3f2780b4cb19d4b
Julian T Skodzinski <juliantsk@gmail.com>
2018-03-12 18:37:45 -0400

    Fixed an issue with topics, began to style the page and title.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,40 @@ var pokemon = ["Psyduck", "Mewtwo", "Pikachu", "Suicune", "Charizard", "Rayquaza
 
 $(document).ready(function() {
     function displayTopics() {
-        $("topics").empty();
+        $("#topics").empty();
         for (var i = 0; pokemon[i]; i++) {
             $("#topics").append($("<button>").addClass("topic").text(pokemon[i]));
-            console.log(pokemon[i]);
         };
     }
+
+    // Title flair
+    function titleBackground() {
+        var random = Math.floor((Math.random() * 100));
+        $.ajax({
+                url: "https://api.giphy.com/v1/gifs/search?api_key=X2pzCnYBHgSnhsVAJTqXkkU1HIOsdjRx&q=pokemon&limit=5&offset=" + random + "&rating=G&lang=en",
+                method: "GET"
+            })
+            .then(function(response) {
+                var results = response.data;
+                var imageURLs = "";
+                var imagePositions = "0vw,";
+                var posNumber = 0;
+
+                for (var i = 0; i < results.length; i++) {
+                    imageURLs += "url(\"" + results[i].images.fixed_height_still.url + "\")";
+                    posNumber += ((results[i].images.fixed_height_still.width / results[i].images.fixed_height_still.height) * 15);
+                    imagePositions += posNumber + "vw";
+                    if (i !== (results.length - 1)) {
+                        imageURLs += ",";
+                        imagePositions += ",";
+                    }
+                }
+                console.log(imageURLs);
+                console.log(imagePositions);
+                $("div.title").css("background-image", imageURLs);
+                $("div.title").css("background-position", imagePositions);
+            });
+    };
 
     function displayGifs() {
         var poke = $(this).text();
@@ -22,34 +50,37 @@ $(document).ready(function() {
                 var results = response.data;
 
                 for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $("<div class='item'>");
+                    var gifDiv = $("<div class='pokemon-gif'>");
 
                     var rating = results[i].rating;
 
                     var p = $("<p>").text("Rating: " + rating);
 
-                    var personImage = $("<img>");
-                    personImage.attr("src", results[i].images.fixed_height.url);
+                    var pokemonImage = $("<img>");
+                    pokemonImage.attr("src", results[i].images.fixed_height.url);
 
                     gifDiv.prepend(p);
-                    gifDiv.prepend(personImage);
+                    gifDiv.prepend(pokemonImage);
 
                     $("#gifs-appear-here").prepend(gifDiv);
-                }
+                };
             });
-    }
+    };
 
+    // Takes the user input and adds it to the list of pokemon.
     $("#add-pokemon").on("click", function(event) {
         event.preventDefault();
         var pokeInput = $("#poke-input").val().trim();
-
         pokemon.push(pokeInput);
 
+        $("#poke-input").val("");
         displayTopics();
+
 
     });
 
     $(document).on("click", ".topic", displayGifs);
 
+    titleBackground();
     displayTopics();
 });
